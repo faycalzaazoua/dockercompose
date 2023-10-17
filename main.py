9ebb40c1616aba61392ee1af227858cab0933545
faycalzaazoua <zaazoua.faycal1@gmail.com>
2023-10-17 19:40:16 +0200

    third commit

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from os import environ as env
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
+# def index():
+#     return { "message": f"Hello {env['MY_VARIABLE']} !"}
 @app.get("/")
-def index():
-    return { "message": f"Hello {env['MY_VARIABLE']} !"}
+def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request, "my_variable": env['name']})
+
+
+# def second_route():
+#     return { "message": "Ceci est la deuxième route."}
+@app.get("/second-route")
+def index(request: Request):
+    return templates.TemplateResponse("annexe.html", {"request": request})
