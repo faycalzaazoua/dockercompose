7b560b6ac84d227cbbbb867f2fc0ba03c07681e0
faycalzaazoua <zaazoua.faycal1@gmail.com>
2023-10-19 17:38:39 +0200

    simple test de container

---
+++ test/test_container.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Définissez le nom du conteneur
+container_name="database-1"
+
+# Commande à exécuter à l'intérieur du conteneur pour vérifier la connectivité (par exemple, ping ou une commande de base de données)
+test_command="ping -c 4 google.com"  # Remplacez par la commande que vous souhaitez exécuter
+
+# Exécutez la commande à l'intérieur du conteneur
+docker exec "$container_name" $test_command
+
+# Vérifiez le code de retour de la commande
+if [ $? -eq 0 ]; then
+    echo "Le conteneur $container_name est fonctionnel."
+else
+    echo "Le conteneur $container_name n'est pas fonctionnel."
+fi
