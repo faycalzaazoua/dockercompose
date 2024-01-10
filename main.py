f3245eaf84eb31c509651cc594f7a9829f3c77c0
faycalzaazoua <zaazoua.faycal1@gmail.com>
2024-01-10 17:01:21 +0100

    commit matin 10/01

--- main.py
+++ main.py
@@ -1,56 +1,47 @@
 from fastapi import FastAPI, Request
-from os import environ as env
 from starlette.templating import Jinja2Templates
 import psycopg2
-
+import uvicorn
+from os import environ as env
 
 app = FastAPI()
-
 templates = Jinja2Templates(directory="templates")
 
-def connect():
-    connection = None
-    db_data = None
-    try:
-        # Connexion a la bdd existante
-        connection = psycopg2.connect(
-            database="microservices",
-            user="docker",
-            password="docker",
-            host="0.0.0.0"
-        )
+# Paramètres de connexion à la base de données
+db_params = {
+    'host': '172.19.0.2',
+    'database': 'microservices',
+    'user': 'docker',
+    'password': 'docker',
+}
 
-        # creation du curseur pour faciliter les opérations vers la bdd
-        crsr = connection.cursor()
-        # Requête à la bdd
-        crsr.execute('SELECT name FROM utilisateur')
-        db_data = crsr.fetchall()
-        crsr.close()
-
-        if db_data is None:
-            print("Aucune donnée trouvée dans la base de données.")
-    except (Exception, psycopg2.DatabaseError) as error:
-        print("Erreur de connexion à la base de données :", error)
-    finally:
-        if connection is not None:
-            # fermer la connexion a la bdd
-            connection.close()
-            print('Connexion à la base de données terminée.')
-
-    return db_data
-print(connect())
-# Création de la première route qui sera l'Acceuil
-# def index():
-#     return { "message": f"Hello {env['MY_VARIABLE']} !"}
 @app.get("/")
 def index(request: Request):
     return templates.TemplateResponse("index.html", {"request": request, "my_variable": env['name']})
 
-
-# Création de la deuxième route qui sera la page annexe
-# def second_route():
-#     return { "message": "Ceci est la deuxième route."}
 @app.get("/second-route")
-def index(request: Request):
-    rows = connect()  # Appel de la fonction connect pour obtenir les données)
-    return templates.TemplateResponse("annexe.html", {"request": request, "datas": rows })
+def second_route(request: Request):
+    try:
+        # Établir la connexion à la base de données
+        conn = psycopg2.connect(**db_params)
+
+        # Créer un curseur
+        cursor = conn.cursor()
+
+        # Exécuter une requête SQL pour récupérer les données de votre table
+        cursor.execute('SELECT * FROM utilisateur')
+
+        # Récupérer les résultats
+        rows = cursor.fetchall()
+
+        # Fermer le curseur et la connexion à la base de données
+        cursor.close()
+        conn.close()
+
+        # Rendre le modèle HTML avec les données récupérées
+        return templates.TemplateResponse("annexe.html", {"request": request, "rows": rows, "my_variable": env['name']})
+
+    except Exception as e:
+        return f"Erreur lors de la connexion à la base de données : {e}"
+
+
