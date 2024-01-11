e4755e86bc863b4e89965487b7484dfa7e959548
faycalzaazoua <zaazoua.faycal1@gmail.com>
2024-01-11 17:08:22 +0100

    commit apres midi jour 2 - /search for elastic route

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from fastapi import FastAPI, Request
 from starlette.templating import Jinja2Templates
 import psycopg2
+from elasticsearch import Elasticsearch
 import uvicorn
 from os import environ as env
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
-# Paramètres de connexion à la base de données
+# Paramètres de connexion à la base de données PostgreSQL
 db_params = {
     'host': '172.19.0.2',
     'database': 'microservices',
     'user': 'docker',
     'password': 'docker',
 }
+
+# Paramètres de connexion à Elasticsearch
+es = Elasticsearch(["http://elasticsearch:9200"])
 
 @app.get("/")
 def index(request: Request):
@@ -22,19 +26,19 @@ def index(request: Request):
 @app.get("/second-route")
 def second_route(request: Request):
     try:
-        # Établir la connexion à la base de données
+        # Établir la connexion à la base de données PostgreSQL
         conn = psycopg2.connect(**db_params)
 
         # Créer un curseur
         cursor = conn.cursor()
 
-        # Exécuter une requête SQL pour récupérer les données de votre table
+        # Exécuter une requête SQL pour récupérer les données de votre table PostgreSQL
         cursor.execute('SELECT * FROM utilisateur')
 
         # Récupérer les résultats
         rows = cursor.fetchall()
 
-        # Fermer le curseur et la connexion à la base de données
+        # Fermer le curseur et la connexion à la base de données PostgreSQL
         cursor.close()
         conn.close()
 
@@ -42,6 +46,16 @@ def second_route(request: Request):
         return templates.TemplateResponse("annexe.html", {"request": request, "rows": rows, "my_variable": env['name']})
 
     except Exception as e:
-        return f"Erreur lors de la connexion à la base de données : {e}"
+        return f"Erreur lors de la connexion à la base de données PostgreSQL : {e}"
 
+@app.get("/search")
+def search(request: Request, query: str):
+    try:
+        # Effectuer une recherche dans Elasticsearch
+        result = es.search(index="votre_index", body={"query": {"match": {"votre_champ": query}}})
 
+        # Rendre le modèle HTML avec les résultats de la recherche
+        return templates.TemplateResponse("search.html", {"request": request, "result": result})
+
+    except Exception as e:
+        return f"Erreur lors de la recherche dans Elasticsearch : {e}"
